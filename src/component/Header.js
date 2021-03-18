d66748e702ff0a0b0c7caaf581627e53d7b2e526
mohamednasr20 <mohamednasr8687@gmail.com>
2021-03-17 23:39:26 -0400

    add activClassName to the navbar

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,29 +2,41 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import logo from '../assets/logo.svg';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import '../styles/Header.css';
 
 const Header = () => {
   return (
     <Navbar className="Navbar pl-0" expand="md">
-      <Link to="/">
+      <NavLink to="/">
         <Navbar.Brand>
           <img src={logo} alt="logo" />
         </Navbar.Brand>
-      </Link>
+      </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="m-md-4 p-3 Navbar-links">
-          <Link className="mr-3 py-4 py-md-0" to="/portfolio">
+          <NavLink
+            className="mr-3 py-4 py-md-0"
+            activeClassName="Navbar-link-active"
+            to="/portfolio"
+          >
             Portfolio
-          </Link>
-          <Link className="mr-3 py-4 py-md-0" to="/about">
+          </NavLink>
+          <NavLink
+            className="mr-3 py-4 py-md-0"
+            activeClassName="Navbar-link-active"
+            to="/about"
+          >
             About Us
-          </Link>
-          <Link className="mr-3 py-4 py-md-0" to="/portfolio">
+          </NavLink>
+          <NavLink
+            className="mr-3 py-4 py-md-0"
+            activeClassName="Navbar-link-active"
+            to="/contact"
+          >
             Contact
-          </Link>
+          </NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
