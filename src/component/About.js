0478717ac8d794b5d1b47848cdd75b8bacddf0c5
mohamednasr20 <mohamednasr8687@gmail.com>
2021-03-31 22:41:58 -0400

    make Hero component reusable

--- src/component/About.js
+++ src/component/About.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Hero from '../component/Hero';
 import CurrentPage from '../component/CurrentPage';
+import hero from '../assets/about/desktop/image-hero.jpg';
+import heroTablet from '../assets/about/tablet/image-hero.jpg';
+import heroMobile from '../assets/about/mobile/image-hero.jpg';
 
 const About = () => {
+  const images = {
+    hero,
+    heroTablet,
+    heroMobile,
+  };
+
+  const description =
+    'Our small team of world-class professionals will work with you every step of the way. Strong relationships are at the core of everything we do. This extends to the relationship our projects have with their surroundings.';
+
   return (
     <section className="About">
       <CurrentPage pageName="about us" />
-      <Hero />
+      <Hero
+        images={images}
+        title="About"
+        subtitle="Your team of professionals"
+        description={description}
+      />
     </section>
   );
 };
--- src/component/Hero.js
+++ src/component/Hero.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import '../styles/Hero.css';
-import hero from '../assets/about/desktop/image-hero.jpg';
-import heroTablet from '../assets/about/tablet/image-hero.jpg';
-import heroMobile from '../assets/about/mobile/image-hero.jpg';
 
-const ShowCase = () => {
+const ShowCase = ({ images, title, subtitle, description }) => {
   return (
     <div className="Hero">
       <div className="Hero-img">
         <picture>
-          <source srcSet={hero} media="(min-width: 1024px)" />
-          <source srcSet={heroTablet} media="(min-width: 768px)" />
-          <img src={heroMobile} alt="hero" />
+          <source srcSet={images.hero} media="(min-width: 1024px)" />
+          <source srcSet={images.heroTablet} media="(min-width: 768px)" />
+          <img src={images.heroMobile} alt="hero" />
         </picture>
       </div>
       <div className="Hero-content-wrapper">
-        <h1 className="Hero-title">About</h1>
+        <h1 className="Hero-title">{title}</h1>
         <div className="Hero-content">
-          <h3>Your team of professionals</h3>
-          <p className="lead">
-            Our small team of world-class professionals will work with you every
-            step of the way. Strong relationships are at the core of everything
-            we do. This extends to the relationship our projects have with their
-            surroundings.
-          </p>
+          <h3>{subtitle}</h3>
+          <p className="lead">{description}</p>
         </div>
       </div>
     </div>
